2708c0a93bec1f08fd0d79816513ff7a21adf586
Ghulam <gsidique37@gmail.com>
2025-08-16 14:49:43 +0500

    Project structure is ready and also created venv and run the requirements.txt file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ghulam Sidique",
+    author_email="gsidique37@gmail.com",
+    packages=find_packages()
+)
